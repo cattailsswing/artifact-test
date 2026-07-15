7e7467460d88193fb91c77c6744ab29b96c09c06
Brooke Chen <brookechen@MacBookAir.fios-router.home>
2026-07-15 17:21:19 -0400

    Add version field

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (_req, res) => {
-  res.json({ service: process.env.SERVICE_NAME || 'artifact-test', status: 'ok' })
+  res.json({ service: process.env.SERVICE_NAME || 'artifact-test', status: 'ok', version: '1.0.1' })
 })
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }))
